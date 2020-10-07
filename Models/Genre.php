290c77ad3060274ebfaa0c86920fb8f4b4f3188a
Juan Ignacio Duhalde <juanignacioduhalde@gmail.com>
2020-10-07 12:58:21 -0300

    create models genre and DAO GenreDAO

---
+++ Models/Genre.php
@@ -0,0 +1,53 @@
+<?php
+namespace Models;
+
+class Genre{
+    private $id,$name;
+
+    public function __construct($id="",$name="")
+    {
+        
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
+
+?>
