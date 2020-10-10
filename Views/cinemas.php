9209dae5ebf2714bcacffad53fae51c2782a7036
Francisco <pancho.dommel@gmail.com>
2020-10-10 16:39:00 -0300

    design CinemaView

--- Views/cinemas.php
+++ Views/cinemas.php
@@ -1,11 +1,20 @@
 <main>
-	<body>
-	<div>
+	<body class="bodyAdministration">
+	<div class="container">
         <?php
-        echo "<pre>";
-        print_r($cinemaArray);
-        echo "<pre>";
-        ?>
+        foreach ($cinemaArray as $value):?>
+        
+        <div class="jumbotron bg-dark text-white">
+                <h1 class="display-4"><?php echo $value->getName(); ?></h1>
+                <p class="lead">Direccion: <?php echo $value->getAddress(); ?></p>
+                <hr class="my-4">
+                <p>Cantidad de Salas: <?php echo count($value->getRoom()); ?></p>
+                <a class="btn btn-outline-success btn-lg" href="#" role="button">Seleccionar</a>
+                <a class="btn btn-outline-primary btn-lg ml-2" href="#" role="button">Editar</a>
+                <a class="btn btn-outline-danger btn-lg ml-2" href="#" role="button">Borrar</a>
+        </div>
+
+        <?php endforeach; ?>
 	</div>
 	</body>
 </main>
--- Views/nav-bar.php
+++ Views/nav-bar.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Movie Pass 2020</a>
+<nav class="navbar-expand-lg navbar navbar-dark bg-dark mb-5">
+  <a class="navbar-brand text-light" href="#">Movie Pass 2020</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,12 +7,12 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Acciones
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="<?php echo FRONT_ROOT; ?>/Movie/GetAll">Listar Peliculas Actuales</a>
-          <a class="dropdown-item" href="<?php echo FRONT_ROOT; ?>/Cinema/GetAll">Administrar Cines</a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item text-light item" href="<?php echo FRONT_ROOT; ?>/Movie/GetAll">Listar Peliculas Actuales</a>
+          <a class="dropdown-item text-light item" href="<?php echo FRONT_ROOT; ?>/Cinema/GetAll">Administrar Cines</a>
         </div>
       </li>
     </ul>
