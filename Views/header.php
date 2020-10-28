1c99cd1fa87d83e4a2541358a68de23e8d58b10e
pvalentinuzzi14 <61996696+pvalentinuzzi14@users.noreply.github.com>
2020-10-28 16:04:45 -0300

    Update header.php
    Fix a problem in stylesheet href with constants

--- Views/header.php
+++ Views/header.php
@@ -3,8 +3,8 @@
     <head>
         <title> MoviePass </title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-sacle=1,minimum-scale=1">
-        <link rel="stylesheet" href="../layout/styles.css">
+        <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1">
+        <link rel="stylesheet" href="<?php echo FRONT_ROOT;?>/layout/styles.css">
         <link href="//db.onlinewebfonts.com/c/0a6ee448d1bd65c56f6cf256a7c6f20a?family=Bahnschrift" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>       
