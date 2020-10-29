a4e6925f3a203e2b8bfad5374cbfe5edf9bc4f8a
Juan Ignacio Duhalde <juanignacioduhalde@gmail.com>
2020-10-29 16:18:28 -0300

    change movie and genre dao from json to data base

--- DAO/Genres.php
+++ DAO/Genres.php
@@ -2,57 +2,108 @@
     namespace DAO;
 
     use Models\Genre as Genre;
+    use PDOException;
 
     class Genres{
-       
-        private $list = array();
-        private $fileName;
+       private $connection;
 
-        public function __construct()
+        /*----------------------------- DB -----------------------------*/
+        public function add(Genre $item)
         {
-            $this->fileName = dirname(__DIR__)."/Data/genres.json";
-        }
-
-        public function GetAll()
-        {
-            $this->RetrieveData();
-            return $this->list;
-        }   
-
-        public function RefreshData()
-        {
-            $arrayToDecode = json_decode(file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=2f0f4f905a5085a4cb6411b8c639165b&language=es-ES'),true);
-
-            foreach($arrayToDecode['genres'] as $valuesArray)
-            {
-                $movie = new Genre();
-                $movie->setId($valuesArray["id"]); 
-                $movie->setName($valuesArray["name"]);
-                array_push($this->list,$movie);
-            }
-
-            $jsonContent = json_encode($arrayToDecode['genres'], JSON_PRETTY_PRINT);
-
-            file_put_contents($this->fileName, $jsonContent);
-        }
-
-
-        private function RetrieveData()
-        {
-            $this->list = array();
-            if(file_exists($this->fileName))
-            {
-                $jsonContent = file_get_contents($this->fileName);
-                $arrayToDecode = ($jsonContent) ? json_decode($jsonContent,true) : array();
-
-                foreach($arrayToDecode as $valuesArray)
-                {
-                    $genre = new Genre();
-                    $genre->setId($valuesArray["id"]);
-                    $genre->setName($valuesArray["name"]);
-                    array_push($this->list,$genre);
+            $value=0;
+                try {
+                    $query = "INSERT INTO genres (id_api_genre,name_genre) VALUES (:id_api_genre,:name_genre)";
+                    $parameters["id_api_genre"] = $item->getId();
+                    $parameters["name_genre"] = $item->getName();
+                    $this->connection = Connection::getInstance();
+                    $value = $this->connection->ExecuteNonQuery($query,$parameters);
+    
+                } catch (PDOException $e) {
+                    throw $e;
                 }
-            }
+                return $value;
         }
+    
+        public function RetrieveDB(){
+            $moviesList = array();
+    
+            try {
+                $sql = "SELECT  * FROM genres";
+                $this->connection = Connection::getInstance();
+                
+                $statement = $this->connection->execute($sql);
+    
+                if(!empty($statement))
+                {
+                    foreach ($statement as $value) {
+                        $movieAux = $this->create($value);
+                        array_push($moviesList,$movieAux);
+                    }
+                }
+            } catch (PDOException $e) {
+                throw $e;
+            }
+    
+            return $moviesList;
+        }
+    
+    
+        //crea model genre 
+        private function create($statementRes)
+        {
+            $genre = new Genre();
+            $genre->setId($statementRes['id_api_genre']);
+            $genre->setName($statementRes['name_genre']);
+            $genre->setIdBd($statementRes['id_genre']);
+            return $genre;
+        }
+    
+        public function getOne($id)
+        {
+            $genre = null;
+    
+            try {
+                $parameters['id_genre'] = $id; 
+    
+                $sql = "SELECT * FROM genres WHERE id_genre=:id";
+                
+                $this->connection = Connection::getInstance();
+                
+                $statement = $this->connection->execute($sql);
+    
+                if(!empty($statement))
+                {
+                    $movie = $this->create($statement[0]);
+                }
+            } catch (PDOException $e) {
+                throw $e;
+            }
+    
+            return $movie;
+        }
+      
+
+    /*----------------------------- API -----------------------------*/
+    public function createFromApi($valuesArray)
+    {
+        $genre = new Genre();
+        $genre->setId($valuesArray["id"]); 
+        $genre->setName($valuesArray["name"]);
+        return $genre;
     }
+
+    public function RetrieveDataFromApi() 
+	{
+		$jsonContent = file_get_contents("https://api.themoviedb.org/3/genre/movie/list?api_key=2f0f4f905a5085a4cb6411b8c639165b&language=es-ES");
+		$arrayToDecode =($jsonContent) ? json_decode($jsonContent,true) : array();
+		$arrayOfGenres = array();
+        
+		foreach($arrayToDecode['genres'] as $value):
+			$genre =$this->createFromApi($value);
+			array_push($arrayOfGenres,$genre);
+		endforeach;	
+
+		return $arrayOfGenres;
+	}
+}
 ?>
