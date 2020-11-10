d7b3a7c28e2868820704db4b79ac966c9d542184
pvalentinuzzi14 <61996696+pvalentinuzzi14@users.noreply.github.com>
2020-11-10 20:07:38 -0300

    cambios erro staged

--- Views/addRoom.php
+++ Views/addRoom.php
@@ -1,5 +1,6 @@
-<main>
-	<body class="bodyAdministration">
+<?php require_once(VIEWS_PATH."header.php"); ?>
+
+<body class="bodyAdministration">
 	<div class="bg-dark container rounded p-3">
     <h2 class="text-light" >Agregar Salas</h2>
         <form  action="<?php echo FRONT_ROOT; ?>/Room/Add" method="POST"> 
@@ -40,4 +41,10 @@
         </form>
 	</div>
 	</body>
-</main>
+    <footer>
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        </body>
+</html>
